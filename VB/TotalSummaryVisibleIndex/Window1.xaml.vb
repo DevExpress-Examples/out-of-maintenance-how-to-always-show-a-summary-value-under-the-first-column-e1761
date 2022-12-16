a4322f1c0090d0b63b7f6407b6002e5d5f0b9821
DevExpressExampleBot <fakeEmail>
2022-12-16 18:24:35 +0400

    Source auto update [skip ci]

--- VB/TotalSummaryVisibleIndex/Window1.xaml.vb
+++ VB/TotalSummaryVisibleIndex/Window1.xaml.vb
@@ -1,34 +1,27 @@
-﻿Imports System.Collections.Generic
+Imports System.Collections.Generic
 Imports System.Windows
 
 Namespace TotalSummaryVisibleIndex
-	Partial Public Class Window1
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-			Dim list As New List(Of SiteInfo)()
-			list.Add(New SiteInfo() With {
-				.Category = "Component vendor",
-				.Hyperlink = "http://devexpress.com",
-				.Name = "Developer Express"
-			})
-			list.Add(New SiteInfo() With {
-				.Category = "Search engine",
-				.Hyperlink = "http://google.com",
-				.Name = "Google"
-			})
-			list.Add(New SiteInfo() With {
-				.Category = "Documentation",
-				.Hyperlink = "http://msdn.com",
-				.Name = "MSDN"
-			})
-			grid.ItemsSource = list
-		End Sub
-	End Class
-	Public Class SiteInfo
-		Public Property Category() As String
-		Public Property Hyperlink() As String
-		Public Property Name() As String
-	End Class
+    Public Partial Class Window1
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+            Dim list As List(Of SiteInfo) = New List(Of SiteInfo)()
+            list.Add(New SiteInfo() With {.Category = "Component vendor", .Hyperlink = "http://devexpress.com", .Name = "Developer Express"})
+            list.Add(New SiteInfo() With {.Category = "Search engine", .Hyperlink = "http://google.com", .Name = "Google"})
+            list.Add(New SiteInfo() With {.Category = "Documentation", .Hyperlink = "http://msdn.com", .Name = "MSDN"})
+            Me.grid.ItemsSource = list
+        End Sub
+    End Class
+
+    Public Class SiteInfo
+
+        Public Property Category As String
+
+        Public Property Hyperlink As String
+
+        Public Property Name As String
+    End Class
 End Namespace
